d3ed5ab4a1d92029da1eb79bd3c299ac2c3bf1fc
Maria Eugenia <101273165+MariEugenia@users.noreply.github.com>
2023-05-03 12:50:12 -0300

    in progress

---
+++ script.js
@@ -0,0 +1,7 @@
+function Colometria() {
+    document.getElementById('texto').style.color="white";
+    document.getElementById('ConteudoCor').style.color="white";
+    document.getElementById('body').style.backgroundColor="red";
+}
+
+
