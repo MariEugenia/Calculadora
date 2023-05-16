4727b74e1fd7c7ff3a73f5141bbce793a91f87f2
Maria Eugenia <101273165+MariEugenia@users.noreply.github.com>
2023-05-16 15:30:40 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,36 @@
-function Colometria() {
-    document.getElementById('texto').style.color="white";
-    document.getElementById('ConteudoCor').style.color="white";
-    document.getElementById('body').style.backgroundColor="red";
+// Obtendo o elemento de exibição
+var display = document.getElementById('display');
+
+// Função para atualizar a exibição
+function updateDisplay(value) {
+  display.value += value;
 }
 
+// Função para calcular o resultado
+function calculate() {
+  var result = eval(display.value);
+  display.value = result;
+}
 
+// Função para limpar a exibição
+function clearDisplay() {
+  display.value = '';
+}
+
+// Obtendo todos os botões
+var buttons = document.getElementsByClassName('button');
+
+// Adicionando um evento de clique a cada botão
+for (var i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener('click', function() {
+    var value = this.getAttribute('data-value');
+
+    if (value === '=') {
+      calculate();
+    } else if (value === 'C') {
+      clearDisplay();
+    } else {
+      updateDisplay(value);
+    }
+  });
+}
